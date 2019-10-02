d056bc14ae285e84e34fcd686755f62e2de5e2aa
Odufa Atsegbua <odufa.atsegbua@gmail.com>
2019-10-02 16:02:49 -0500

    saving intial files

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// first we need to set up the function that will grab the data we want when the buttons are clicked
+$(document).ready(function() { 
+
+    var topics = ["drake", "rihanna", "travis scott", "kanye west", "kid cudi", "the weeknd"];
+
+
+
+
+
+
+
+
+
